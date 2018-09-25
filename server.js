40b43cba6e9c0acc1e6173ae1be69ed543cd739d
avidrutham <divyamathur89@gmail.com>
2018-09-25 16:12:49 +0530

    add url-parse module & post route

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var mongo = require('mongodb');
 var mongoose = require('mongoose');
 var cors = require('cors');
 var bodyParser = require('body-parser');
+var dns = require('dns');
 
 mongoose.Promise = global.Promise;
 
@@ -28,20 +29,27 @@ app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function(req, res){
+app.get('/', (req, res) =>{
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
   
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
+app.get("/api/hello", (req, res) => {
   res.json({greeting: 'hello API'});
 });
 
+// Response to post request
+app.post('/api/shorturl/new', (req, res) => {
+  var url = req.body.url;
+  res.send(url);
+});
 
-app.listen(port, function () {
+
+app.listen(port, () => {
   console.log('Node.js listening ...');
 });
